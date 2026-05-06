0677ed4178a2517489f38401ab3609bb7ef4c8ba
lu1s__ <atencioluis206@gmail.com>
2026-05-06 12:44:00 +0800

    database changes

--- data/db.js
+++ data/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST || process.env.MYSQL_HOST || 'localhost',
-  user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || 'lu1smysql',
+  user: process.env.MYSQLUSER || process.env.MYSQL_USER || 'fetchit_user',
+  password: process.env.MYSQLPASSWORD || process.env.MYSQL_PASSWORD || 'FetchIt@2024!',
   database: process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE || 'fetchit',
   port: process.env.MYSQLPORT || process.env.MYSQL_PORT || 3306
 });
